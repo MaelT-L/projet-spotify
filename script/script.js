7b09cc787f036b0b7fab621c76e68ded0e6b4077
MaelT-L <mael.thingleoh@gmail.com>
2025-05-13 11:00:17 +0200

    Start the css

--- script/script.js
+++ script/script.js
@@ -77,26 +77,31 @@ fetch('data/data.json')
     new Chart(ctx, {
       type: 'pie',
       data: {
-        labels: labels,
-        datasets: [{
-          label: "Distribution des genres",
-          data: values,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-            'rgba(153, 102, 255, 0.6)',
-            'rgba(255, 159, 64, 0.6)',
-            'rgba(89, 128, 205, 0.6)',
-            'rgba(128, 128, 128, 0.6)'
-          ],
-          borderColor: 'white',
-          borderWidth: 1
-        }]
+      labels: labels,
+      datasets: [{
+        label: "Distribution des genres",
+        data: values,
+        backgroundColor: [
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(89, 128, 205, 0.6)',
+        'rgba(128, 128, 128, 0.6)'
+        ],
+        borderColor: 'white',
+        borderWidth: 1
+      }]
       },
       options: {
-        responsive: true
+      responsive: true,
+      plugins: {
+        legend: {
+        position: 'right'
+        }
+      }
       }
     });
   })
